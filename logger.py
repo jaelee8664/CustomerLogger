ebcb7d38557b949dfc6fea58b10925a8dbccf91a
jaelee8664 <93635286+jaelee8664@users.noreply.github.com>
2025-05-05 01:51:37 +0900

    Update logger.py

--- logger.py
+++ logger.py
@@ -12,42 +12,45 @@ class CustomerLogger:
     _logger_list = {} # Dictionary to store different types of loggers
     _instance = None # Class variable to hold the singleton instance
     
-    def __new__(cls, logger_name:list[str], console:bool = False):
+    def __new__(cls):
         # Use the Singleton pattern to ensure only one instance is created
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            
-            # Create a folder named 'log' if it doesn't already exist
-            if not os.path.exists("log"):
-                os.makedirs("log")
-            
-            # Set up the log formatter
-            formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-            
-            # Create loggers
-            for name in logger_name:
-                logger = logging.getLogger(name)
-                cls._logger_list.update({name: logger})
-                
-                file_handler = logging.FileHandler(f"log/{name}_info.log")
-                file_handler.setLevel(logging.INFO)
-                file_handler.setFormatter(formatter)
-                logger.addHandler(file_handler)
-                
-                # Add error level file handler
-                error_file_handler = logging.FileHandler(f"log/{name}_error.log")
-                error_file_handler.setLevel(logging.ERROR)
-                error_file_handler.setFormatter(formatter)
-                logger.addHandler(error_file_handler)
-                
-                # Add console handler if specified
-                if console:
-                    console_handler = logging.StreamHandler(sys.stdout)
-                    console_handler.setLevel(logging.DEBUG)
-                    console_handler.setFormatter(formatter)
-                    logger.addHandler(console_handler)
 
         return cls._instance
+    
+    @classmethod
+    def init(cls, logger_name:str, console:bool = False):
+        
+        # Create a folder named 'log' if it doesn't already exist
+        if not os.path.exists("log"):
+            os.makedirs("log")
+        
+        # Set up the log formatter
+        formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        
+        # Create loggers
+        for name in logger_name:
+            logger = logging.getLogger(name)
+            cls._logger_list.update({name: logger})
+            
+            file_handler = logging.FileHandler(f"log/{name}_info.log")
+            file_handler.setLevel(logging.INFO)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+            
+            # Add error level file handler
+            error_file_handler = logging.FileHandler(f"log/{name}_error.log")
+            error_file_handler.setLevel(logging.ERROR)
+            error_file_handler.setFormatter(formatter)
+            logger.addHandler(error_file_handler)
+            
+            # Add console handler if specified
+            if console:
+                console_handler = logging.StreamHandler(sys.stdout)
+                console_handler.setLevel(logging.DEBUG)
+                console_handler.setFormatter(formatter)
+                logger.addHandler(console_handler)
     
     @classmethod
     def trace_call(cls, logger_name:str, to_log: Optional[str] = None):
@@ -76,7 +79,7 @@ class CustomerLogger:
                     for arg_name, arg_value in func_args.items():
                         logger.info(f"{arg_name}: {arg_value}")
                         
-                    logger.info(f"Result: {result}")
+                    logger.info(f"Return: {result}")
                     
                     logger.info(f"Additinal message: {to_log}") if to_log else None
                 except Exception as e:
@@ -99,7 +102,7 @@ if __name__ == "__main__":
     TEST CODE
     """
     # Create the CustomerLogger singleton instance
-    CustomerLogger(logger_name = ["test1", "test2"], console=True)
+    CustomerLogger.init(logger_name = ["test1", "test2"], console=True)
     
     @CustomerLogger.trace_call(logger_name="test1", to_log="This a test info log")
     def test_info_function(a, b, list_):
